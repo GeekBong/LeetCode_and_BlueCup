3c02f2facb7f8b76457768c4faab45c1169ac83e
GeekBong <2132968080@qq.com>
2024-11-06 23:28:17 +0800

    Blue1259.奇怪的捐赠

---
+++ LeetCode_and_BlueCup_Code/Blue1259.奇怪的捐赠.cpp
@@ -0,0 +1,12 @@
+//#include <iostream>
+//using namespace std;
+//int main() {
+//	int b = 1000000;
+//	int res = 0;
+//	while(b){
+//		res += b % 7;
+//		b /= 7;
+//	}
+//	cout << res;
+//	return 0;
+//}
